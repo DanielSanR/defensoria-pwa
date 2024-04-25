bf6d12fe1032fd2985eb5b3c97b62007081850d1
silicon <daniel.romero@siliconmisiones.gob.ar>
2024-04-25 03:34:17 -0300

    updates ui

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,7 +8,7 @@ import { Subscription } from 'rxjs';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage implements OnInit,OnDestroy { 
+export class HomePage  { 
   values: any[]=[
     {id:1,text:'Pedir Ayuda',icon:'microfono.svg',page:'/principal/formulario',externalURL:false},
     {id:2,text:'Consejos',icon:'faq.svg',page:'/principal/preguntas-frecuentes',externalURL:false},
@@ -17,25 +17,11 @@ export class HomePage implements OnInit,OnDestroy {
   formRoute: string;
   $obs= new Subscription();
   constructor(public formService: FormService,public router: Router) {}
-   ngOnInit() {
-    const sub = this.formService.getSelectedData().subscribe(
-      res=>{
-        this.formRoute= (res==='teen' && res != null) ? this.values[0].page ='/formulario' : this.values[0].page ='/formulario-kids';
-      }
-    );
-    this.$obs.add(sub);
-  }
-  ngOnDestroy(){
-    this.$obs.unsubscribe();
-  }
-
-  redirect(item: any){ 
-    console.log(item)
+   
+  redirect(item: any){  
     if(item.externalURL){
-
       location.assign(item.page);
     }
-
     this.router.navigate([item.page]);
   }
 }
