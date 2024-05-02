d52648ed2d2c04f3bfd9615fab185255c31d7772
silicon <daniel.romero@siliconmisiones.gob.ar>
2024-05-02 19:02:36 -0300

    update form animations

--- src/app/form/form.page.ts
+++ src/app/form/form.page.ts
@@ -1,27 +1,35 @@
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
-import { OnInit,ChangeDetectorRef, Component, ViewChild, ViewEncapsulation, ElementRef } from '@angular/core';
+import { OnInit, ChangeDetectorRef, Component, ViewChild, ElementRef, QueryList, AfterViewInit, ViewChildren } from '@angular/core';
 import { Router } from '@angular/router';
-import { RangeCustomEvent, LoadingController } from '@ionic/angular';
+import { LoadingController } from '@ionic/angular';
 import PreguntasformArrKid from '../../assets/datos/DinamicFormKid.json';
 import PreguntasformArr from '../../assets/datos/DinamicForm.json';
 import { Help } from '../models/Help';
 import { FormService } from '../services/form.service';
-import { DeviceService } from '../services/device.service';
-import { SendmailService } from '../services/sendmail.service';
-import { FormBuilder, FormGroup, FormArray,FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl, FormArray } from '@angular/forms';
+import type { Animation } from '@ionic/angular';
+import { AnimationController } from '@ionic/angular';
+import type { IonLabel } from '@ionic/angular';
 export interface PreguntasForm {
   key: string;
   title: string;
   type: string;
   multiple: boolean;
   preguntas?: Array<OpcForm>;
-  icon?:boolean
+  icon?: boolean
 }
 interface OpcForm {
-id: number;
-val: string;
-isChecked?: boolean | false;
-icon?:string;
+  id: number;
+  val: string;
+  isChecked?: boolean | false;
+  icon?: string;
+  inputs?: Array<Inputs>;
+}
+
+interface Inputs {
+  key?: string;
+  type?: string;
+  label?: string;
 }
 
 @Component({
@@ -29,15 +37,19 @@ icon?:string;
   templateUrl: './form.page.html',
   styleUrls: ['./form.page.scss'],
 })
-export class FormPage implements OnInit {
+export class FormPage implements OnInit, AfterViewInit {
   private swiperInstance: any;
   @ViewChild('swiper')
   set swiper(swiperRef: ElementRef) {
     setTimeout(() => {
       this.swiperInstance = swiperRef.nativeElement.swiper;
-      
+
     }, 0);
-  } 
+  }
+  @ViewChildren('preguntasList', { read: ElementRef }) preguntasListRef: QueryList<ElementRef>
+  @ViewChild('titulo', { read: ElementRef }) tituloRef: ElementRef<HTMLIonLabelElement>;
+  @ViewChild('icono', { read: ElementRef }) iconoRef: ElementRef<HTMLIonIconElement>;
+  @ViewChild('itemVictimaOpcionalParaMi', { read: ElementRef }) itemVictimaOpcionalRef: ElementRef<HTMLIonItemElement>;
   edadesDisponibles: number[] = [];
   edadSeleccionada: number = 0;
   public myForm: FormGroup;
@@ -46,24 +58,24 @@ export class FormPage implements OnInit {
   public bandera = false;
   public selected: boolean = false;
   public selectedAge: any;
-  public background ='#ffff';
+  public background = '#ffff';
   public finalized: boolean = false;
-  public backgroundChecked='#5A4BB2';
-  public rangoEtario:string;
+  public backgroundChecked = '#5A4BB2';
+  public rangoEtario: string;
   isckecked: boolean = false;
-  constructor(private router: Router,private formService: FormService,
-    private mailService: SendmailService,
-    private cd: ChangeDetectorRef, private loadingController: LoadingController,
-    private fb: FormBuilder) { 
-      this.inicializarEdades(4, 18);
-      this.myForm = this.fb.group({});
-    }
+  animation: Animation;
+  constructor(private router: Router, private formService: FormService,
+    private loadingController: LoadingController,
+    private fb: FormBuilder, private animationCtrl: AnimationController) {
+    this.inicializarEdades(4, 18);
+    this.myForm = this.fb.group({});
+  }
 
   ngOnInit() {
-    
+
     this.formService.getSelectedData().subscribe((data) => {
       this.rangoEtario = data;
-      this.dinamicForm = this.rangoEtario === 'teen' ? PreguntasformArr : PreguntasformArrKid;
+      this.dinamicForm = PreguntasformArr
     });
     this.createControls(this.dinamicForm);
   }
@@ -75,20 +87,20 @@ export class FormPage implements OnInit {
   seleccionarEdad(edad: number) {
     this.edadSeleccionada = edad;
     this.myForm.get('edad').setValue(edad);
-    this.selected=true
+    this.selected = true
   }
- 
+
   createControls(controls: Array<PreguntasForm>) {
     controls.forEach(control => {
       if (control.key === 'edad') {
         const edadControl = this.fb.control(5, [Validators.required, Validators.min(5), Validators.max(17)]);
         this.myForm.addControl(control.key, edadControl);
-      } 
+      }
       else if (control.multiple) {
         const multipleControl = this.fb.control([], this.minSelectedCheckboxes(1));
         this.myForm.addControl(control.key, multipleControl);
-          
-      } 
+
+      }
       else {
         const formControl = this.fb.control('', Validators.required);
         this.myForm.addControl(control.key, formControl);
@@ -103,151 +115,301 @@ export class FormPage implements OnInit {
     return validator;
   }
 
-  getFormArray(formArrayName: string): FormArray {
-    return this.myForm.get(formArrayName) as FormArray;
-  }
-
-  updateSelections(key: string, value: string, isChecked: boolean) {
-  const selections = this.myForm.get(key).value as Array<string>;
-  if (isChecked) {
-    this.myForm.get(key).setValue([...selections, value]);
-   } 
-  else {
-    this.myForm.get(key).setValue(selections.filter(v => v !== value));
-   }
-  }
-  
-  submitForm(){
-    console.log(this.myForm.value);
-    
+  submitForm() {
+    /* this.dinamicForm.forEach(group => {
+      if (group.multiple) {
+        const control = this.myForm.get(group.key) as FormArray;
+        group.preguntas.forEach(pregunta => {
+          if (pregunta.isChecked) {
+            control.push(this.fb.control(pregunta.val)); 
+            if (pregunta.inputs && pregunta.inputs.length > 0) {
+              pregunta.inputs.forEach(input => {
+                const inputControl = this.myForm.get(input.key);
+                if (inputControl && inputControl.value) { 
+                  control.push(this.fb.control(inputControl.value));
+                }
+              });
+            }
+          }
+        });
+      }
+    }); */
   }
 
   toggleSelection(key: string, val: string) {
     const groupIndex = this.dinamicForm.findIndex(group => group.key === key);
     const group = this.dinamicForm[groupIndex];
+    let wasDeselected = false;
     if (group.multiple) {
       const preguntaIndex = group.preguntas.findIndex(pregunta => pregunta.val === val);
+      const pregunta = group.preguntas[preguntaIndex]
       group.preguntas[preguntaIndex].isChecked = !group.preguntas[preguntaIndex].isChecked;
-    } 
-    else {
+      if(pregunta.isChecked){
+        this.manageInputs(pregunta,key)
+      }
+    } else {
+      
       group.preguntas.forEach(pregunta => {
-        pregunta.isChecked = pregunta.val === val ? !pregunta.isChecked : false;
-       });
+        if (pregunta.val === val && pregunta.isChecked) {
+          wasDeselected = true; 
+          pregunta.isChecked = false;
+        } else {
+          pregunta.isChecked = (pregunta.val === val);
+        } 
+        const preguntaContainer = document.getElementById(`question-container-${pregunta.id}`);
+        if (preguntaContainer) { 
+          if (pregunta.isChecked) {
+            this.animateAndShow(preguntaContainer);
+            this.manageInputs(pregunta,key)
+          } else {
+            if( wasDeselected){ 
+                  this.resetVisibility(preguntaContainer);
+            }
+            else {this.hideElement(preguntaContainer);}
+          }
+        }
+      });
     }
-    if(this.swiperInstance.activeIndex === 6){
-      this.enableBandera(key,val);
-      console.log("entro en enable")
+    if (wasDeselected) {
+      console.log("fue true")
+      this.resetAllElementsVisibility(group);
+    }
+    if (this.swiperInstance.activeIndex === 6) {
+      this.enableBandera(key, val);
     }
     this.selected = this.filterItems(group.preguntas);
     this.updateFormControl(key, group.preguntas);
   }
-   
-  updateFormControl(key: string, preguntas: any[]) {
-    const selectedValues = preguntas.filter(pregunta => pregunta.isChecked).map(pregunta => pregunta.val);
-    this.myForm.get(key).setValue(selectedValues);
+  animateAndShow(element: HTMLElement) {
+    this.animationCtrl.create()
+      .addElement(element)
+      .duration(700)
+      .easing('ease-in')
+      /* .fromTo('transform', 'translateY(0)', 'translateY(-50px)') */
+     /*   .afterStyles({
+        'z-index': -1
+       }) */
+      .play();
+  }
+  
+  hideElement(element: HTMLElement) {
+    this.animationCtrl.create()
+      .addElement(element)
+      .duration(300)
+      .fromTo('opacity', '1', '0')
+      .afterStyles({
+        'display': 'none',
+        'pointer-events': 'none'
+       }) 
+      .play();
   }
 
-  isChecked(key: string, value: string): boolean {
-    const currentValues = this.myForm.get(key).value || [];
-    return currentValues.includes(value);
+  resetVisibility(element: HTMLElement) {
+    this.animationCtrl.create()
+      .addElement(element)
+      .duration(300)
+      .easing('ease-in')
+      .fromTo('opacity', '0', '1')
+      .afterStyles({
+        'display': 'block', 
+        'pointer-events': 'auto'   
+      })
+      .play();
   }
+  
+  resetAllElementsVisibility(group: any) {
+    group.preguntas.forEach(pregunta => {
+      const container = document.getElementById(`question-container-${pregunta.id}`);
+      if (container && !pregunta.isChecked) {
+        this.resetVisibility(container);
+      }
+    });
+  }
+  
+  manageInputs(pregunta, key) {
+    if (pregunta.isChecked && pregunta.inputs.length > 0) {
+      pregunta.inputs.forEach(input => {
+        if (!this.myForm.contains(input.key)) {
+          this.myForm.addControl(input.key, this.fb.control(''));
+        }
+      });
+    } else {
+      pregunta.inputs.forEach(input => {
+        if (this.myForm.contains(input.key)) {
+          this.myForm.removeControl(input.key);
+          this.myForm.get(input.key).reset();
+        }
+      });
+    }
+    console.log("form actual:", this.myForm.value)
+  }
+
+  updateFormControl(key: string, preguntas: any[]) {
+    const selectedValues = preguntas.filter(pregunta => pregunta.isChecked).map(pregunta => {
+      if (pregunta.inputs && pregunta.inputs.length > 0 && this.myForm.get(pregunta.inputs[0].key)) {
+        return this.myForm.get(pregunta.inputs[0].key).value;
+      }
+      return pregunta.val;
+    });
+    if (this.myForm.contains(key)) {
+      this.myForm.get(key).setValue(selectedValues);
+    } else {
+      console.error(`El control con la clave '${key}' no existe en el FormGroup.`);
+    }
+
+  }
+
   ionViewDidEnter() {
-    console.log('ionViewDidEnter'); 
-    this.background ='#FFF';
+    this.initListAnimation();
+    this.background = '#FFF';
     this.finalized = false;
   }
+  ngAfterViewInit() {
 
-  next(){ 
-    this.selected = false;
-    if(this.swiperInstance.activeIndex === this.swiperInstance.slides.length - 1){
+  }
+  initListAnimation() {
+    const itemRefArray = this.preguntasListRef.toArray();
+    for (let i = 0; i < itemRefArray.length; i++) {
+      const element = itemRefArray[i].nativeElement;
+
+      this.animationCtrl
+        .create()
+        .addElement(element)
+        .duration(900)
+        .delay(i * (50 / 3))
+        .easing('cubic-bezier(0.4, 0.0, 0.2, 1.0)')
+        .fromTo('transform', 'translateY(50px)', 'translateY(0px)')
+        .fromTo('visibility', 'hidden', 'visible')
+        .fromTo('opacity', '0', '1')
+        .play();
     }
-    else{
+    /* const itemRefArray = this.preguntasListRef.toArray();
+    for (let i = 0; i < itemRefArray.length; i++) {
+      const element = itemRefArray[i].nativeElement;
+
+      this.animationCtrl
+        .create()
+        .addElement(element)
+        .duration(1000)
+        .delay(i * (1000 / 3)) 
+        .fromTo('transform', 'translateY(50px)', 'translateY(0px)')
+        .fromTo('opacity', '0', '1')
+        .play();
+    } */
+  }
+
+  //ANIMACION PARA VICTIMA ITEM OPCIONAL 
+
+  initAnimationVictima(toggle: boolean) {
+    const visibility = toggle ? 'visible' : 'hidden';
+    const opacity = toggle ? '1' : '0';
+    const itemRefArray = this.itemVictimaOpcionalRef.nativeElement;
+    this.animationCtrl
+      .create()
+      .addElement(itemRefArray)
+      .duration(900)
+      .easing('cubic-bezier(0.4, 0.0, 0.2, 1.0)')
+      .fromTo('transform', 'translateY(50px)', 'translateY(0px)')
+      .fromTo('visibility', visibility, visibility === 'visible' ? 'hidden' : 'visible')
+      .fromTo('opacity', opacity, opacity === '1' ? '0' : '1')
+      .play();
+  }
+
+  next() {
+    this.selected = false;
+    if (this.swiperInstance.activeIndex === this.swiperInstance.slides.length - 1) {
+    }
+    else {
       this.swiperInstance.slideNext(500);
     }
   }
-  prev(){
-    if(this.swiperInstance.activeIndex ===0){
+  prev() {
+    if (this.swiperInstance.activeIndex === 0) {
       this.close();
     }
+    this.swiperInstance.slidePrev(500);;
+  }
+
+  close() {
     this.selected = false;
-    this.swiperInstance.slidePrev(500);;  
-    }
-   
-  close(){
-    this.selected = false;
-    this.swiperInstance.slideTo(0, 500,false);
+    this.swiperInstance.slideTo(0, 500, false);
     this.swiperInstance.update();
     this.resetForm()
     this.router.navigateByUrl('/principal/inicio');
   }
 
+
+
   filterItems(arr: any) {
     return arr.filter((el: any) => el).map((e: any) => e.isChecked).includes(true);
   }
 
-  enableBandera(key: string, val: string){
+  enableBandera(key: string, val: string) {
     const groupIndex = this.dinamicForm.findIndex(group => group.key === key);
     const group = this.dinamicForm[groupIndex];
-    const isSelected= this.filterItems(group.preguntas);
-    if(isSelected){
+    const isSelected = this.filterItems(group.preguntas);
+    if (isSelected) {
       this.bandera = true;
       this.selected = true;
-    } 
-    else{ 
+    }
+    else {
       this.bandera = false;
     }
 
   }
-  
-  async backHome(){
+
+  async backHome() {
     const loading = await this.loadingController.create({
       message: 'Cerrando Formulario...',
       spinner: 'circles',
     });
     await loading.present();
     this.finalized = false;
-     this.swiperInstance.slideTo(0, 500,false);
-     this.swiperInstance.update();
-     this.resetForm()
-       loading.dismiss();
+    this.swiperInstance.slideTo(0, 500, false);
+    this.swiperInstance.update();
+    this.resetForm()
+    loading.dismiss();
   }
-  
-  async enviar(){
-    this.bandera= false;
+
+  async enviar() {
+    this.bandera = false;
     console.log(this.myForm.value);
-    /* const t = await this.mailService.sendForm('form', this.myForm.value); */
     const t = true;
-    if(t){
+    if (t) {
       this.swiperInstance.slideNext(500);
       this.swiperInstance.update();
       this.finalized = true;
     }
-    else { 
+    else {
       this.finalized = false;
     }
   }
 
   async ionViewWillLeave() {
-    console.log('ionViewWillLeave');
     const loading = await this.loadingController.create({
       message: 'Cerrando Formulario...',
       spinner: 'circles',
     });
     await loading.present();
     this.finalized = false;
-    this.swiperInstance.slideTo(0, 500,false);
+    this.swiperInstance.slideTo(0, 500, false);
     this.resetForm()
-    loading.dismiss(); 
+    loading.dismiss();
   }
 
-  resetForm(){
+  resetForm() {
     this.myForm.reset();
     this.dinamicForm.forEach(group => {
-      if(group.type === 'item'){
+      if (group.type === 'item') {
+        this.resetAllElementsVisibility(group)
         group.preguntas.forEach(pregunta => {
           pregunta.isChecked = false;
         });
       }
     });
+  }
+
+  test(e: Event) {
+    /*      this.initListAnimation();  */
   }
 }
