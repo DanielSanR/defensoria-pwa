746e9b895701c2d5cd414035779ab608c27cf52a
silicon <daniel.romero@siliconmisiones.gob.ar>
2024-08-15 16:39:37 -0300

    merge features/Joel

--- src/app/denuncias/denuncias.page.ts
+++ src/app/denuncias/denuncias.page.ts
@@ -9,13 +9,17 @@ import { DenunciasService } from '../services/denuncias.service';
   styleUrls: ['./denuncias.page.scss'],
 })
 export class DenunciasPage {
-  denunciasArray=  [{ 'id_denuncia': '1', 'fecha': '11/12/2024', 'denuncia': 'Denuncia 1', 'estado': 'En proceso', 'victima': 'Victima 1','chip':'#f4f4f4','chip_color':'#000','dni':'12345678','telefono':'123456789','email':'emailtest@gmail.com','direccion':'direccion test','descripcion':'descripcion test'},
+ /*  denunciasArray=  [{ 'id_denuncia': '1', 'fecha': '11/12/2024', 'denuncia': 'Denuncia 1', 'estado': 'En proceso', 'victima': 'Victima 1','chip':'#f4f4f4','chip_color':'#000','dni':'12345678','telefono':'123456789','email':'emailtest@gmail.com','direccion':'direccion test','descripcion':'descripcion test'},
     { 'id_denuncia': '2', 'fecha': '12/12/2024', 'denuncia': 'Denuncia 2', 'estado': 'En proceso', 'victima': 'Victima 2','chip':'#f4f4f4','chip_color':'#000','dni':'12345678','telefono':'123456789','email':'emailtest@gmail.com','direccion':'direccion test','descripcion':'descripcion test'},
     { 'id_denuncia': '3', 'fecha': '13/12/2024', 'denuncia': 'Denuncia 3', 'estado': 'Finalizado', 'victima': 'Victima 3','chip':'#5A4BB2','chip_color':'#fff','dni':'12345678','telefono':'123456789','email':'emailtest@gmail.com','direccion':'direccion test','descripcion':'descripcion test'}
-  ]
+  ] */
+    denunciasArray=  [{ 'id_denuncia': '1', 'fecha': '11/12/2024', 'denuncia': 'Denuncia 1', 'estado': 'En proceso', 'victima': 'Victima 1','chip':'#f4f4f4','chip_color':'#000','dni':'12345678','telefono':'123456789','email':'emailtest@gmail.com','direccion':'direccion test','descripcion':'descripcion test'},
+    { 'id_denuncia': '2', 'fecha': '12/12/2024', 'denuncia': 'Denuncia 2', 'estado': 'En proceso', 'victima': 'Victima 2','chip':'#f4f4f4','chip_color':'#000','dni':'12345678','telefono':'123456789','email':'emailtest@gmail.com','direccion':'direccion test','descripcion':'descripcion test'},
+    { 'id_denuncia': '3', 'fecha': '13/12/2024', 'denuncia': 'Denuncia 3', 'estado': 'Finalizado', 'victima': 'Victima 3','chip':'#5A4BB2','chip_color':'#fff','dni':'12345678','telefono':'123456789','email':'emailtest@gmail.com','direccion':'direccion test','descripcion':'descripcion test'}
+  ] 
+  
   constructor(private route: Router,private denunciaService: DenunciasService) { }
 
- 
 
   details(denuncia){
     this.denunciaService.setDenuncias(denuncia);
--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -47,16 +47,17 @@ async getSelected(): Promise<boolean>{
 async updateForm(form: any){ 
   await this.deviceService.updateDeviceForm(form);
   const device = JSON.parse(await this.storageService.getStorage2('device'));
-  let deviceForm = {device: {uuid: device.uuid,form: form}};
+/*   let deviceForm = {device: {uuid: device.uuid,form: form}}; */
+  let deviceForm = {userYoCuento: { uuid: device.uuid,"latlong": device.latlng,"ageRange": device.ageRange},"quiz" : device.quiz}
   console.log("POST para Formulario",deviceForm);
-  /* let request = this.http.post(`${this.url}/Auth/denuncias/guardar`,device).toPromise().then((res: any) => {
+   let request = this.http.post(`${this.url}Auth/denuncias/guardar`,deviceForm).toPromise().then((res: any) => {
     if (res.status === 200) {
       return true;
     }
   }).catch(err => {
      return false;
    });
-   return request  */
+   return request
     return true;
 }
 
